74e86af57a0f32dbc6b95dac1470160d61be571c
Cory Potter <pottercory22@gmail.com>
2020-09-09 13:39:19 -0500

    added api keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "ed4db63be0b05044afddd7c35890d616"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCjc5j3spClypBnA-h2Y8JEfF20qHf85ew"
